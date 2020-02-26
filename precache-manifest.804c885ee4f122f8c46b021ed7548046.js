34ba181ee2ea13512e8015beaa85fe847027445f
mjedg3 <57395358+mjedg3@users.noreply.github.com>
2020-02-26 15:02:46 +0000

    Updates

---
+++ precache-manifest.804c885ee4f122f8c46b021ed7548046.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbb5657978c49bc69a582b858e3453e",
+    "url": "/gfont-clone-challenge/npm/index.html"
+  },
+  {
+    "revision": "8c0d995547083d9417c9",
+    "url": "/gfont-clone-challenge/npm/static/css/main.2b1b77d2.chunk.css"
+  },
+  {
+    "revision": "8785dd53fd85e5ac2769",
+    "url": "/gfont-clone-challenge/npm/static/js/2.c33bf1e9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gfont-clone-challenge/npm/static/js/2.c33bf1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c0d995547083d9417c9",
+    "url": "/gfont-clone-challenge/npm/static/js/main.4535cb05.chunk.js"
+  },
+  {
+    "revision": "461dadb7921f7c8336bb",
+    "url": "/gfont-clone-challenge/npm/static/js/runtime-main.635dcc2e.js"
+  },
+  {
+    "revision": "ace77d1b968c50fbe27fee3c1860bb34",
+    "url": "/gfont-clone-challenge/npm/static/media/plus.ace77d1b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gfont-clone-challenge/.src/precache-manifest.1e2b4d269cb38c98fc20f76eb967bece.js"
+  "/gfont-clone-challenge/npm/precache-manifest.804c885ee4f122f8c46b021ed7548046.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gfont-clone-challenge/.src/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gfont-clone-challenge/npm/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
